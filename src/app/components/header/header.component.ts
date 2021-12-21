637730b4d5da263c80bc14f83eaf3b15d03ffe32
ReaperClown <the.willian.mesquita@gmail.com>
2021-12-21 12:04:13 -0300

    Raw deployment, awaiting customization

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as jquery from 'jquery';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
